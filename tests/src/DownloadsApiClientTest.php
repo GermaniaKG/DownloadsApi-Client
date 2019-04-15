2a948854a3a6c2d032e462727609e6b014fab629
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-04-15 16:54:43 +0200

    Make test complete

--- tests/src/DownloadsApiClientTest.php
+++ tests/src/DownloadsApiClientTest.php
@@ -2,7 +2,11 @@
 namespace tests;
 
 use Germania\DownloadsApiClient\DownloadsApiClient;
+use Germania\DownloadsApiClient\DownloadsApiClientUnexpectedValueException;
 use GuzzleHttp\Client;
+use Prophecy\Argument;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
 
 class DownloadsApiClientTest extends \PHPUnit\Framework\TestCase
 {
@@ -25,6 +29,8 @@ class DownloadsApiClientTest extends \PHPUnit\Framework\TestCase
 		$this->cachepool = new \Stash\Pool( new \Stash\Driver\FileSystem([
 			'path' => dirname(dirname(__FILE__)) . "/cache/"
 		]));
+
+		parent::setUp();
 	}
 
 
@@ -41,8 +47,64 @@ class DownloadsApiClientTest extends \PHPUnit\Framework\TestCase
 
 		$latest = $sut->latest([  "product" => "plissee" ]);
 		$this->assertInstanceOf( \Traversable::class, $latest);
-
-
 	}
+
+
+	public function testEmptyIteratorResultOnRequestException()
+	{
+		$exception = $this->prophesize( ClientException::class );
+
+		$client = $this->prophesize( Client::class );
+		$client->get( Argument::type("string"), Argument::type("array"))->willThrow( $exception->reveal() );
+		$client_stub = $client->reveal();
+
+		$sut = new DownloadsApiClient( $client_stub );
+
+		$all = $sut->all([ 
+			"product" => "plissee",
+			"category" => "montageanleitung" 
+		]);
+		$this->assertInstanceOf( \Traversable::class, $all);
+		$this->assertEquals( 0, count($all));
+
+		$latest = $sut->latest([  "product" => "plissee" ]);
+		$this->assertInstanceOf( \Traversable::class, $latest);
+		$this->assertEquals( 0, count($latest));
+	}
+
+
+	/**
+	 * @dataProvider provideVariousInvalidResonseBodies
+	 */
+	public function testExceptionOnWeirdResponseBody( $body )
+	{
+		$response = new Response( 200, array(), $body );
+
+		$client = $this->prophesize( Client::class );
+		$client->get( Argument::type("string"), Argument::type("array"))->willReturn( $response );
+		$client_stub = $client->reveal();
+
+		$sut = new DownloadsApiClient( $client_stub );
+
+		$this->expectException( DownloadsApiClientUnexpectedValueException::class );
+		$all = $sut->all();
+
+		$this->expectException( DownloadsApiClientUnexpectedValueException::class );
+		$latest = $sut->latest([  "product" => "plissee" ]);
+	}
+
+	public function provideVariousInvalidResonseBodies()
+	{
+		return array(
+			[ "hello!" ],
+			[ json_encode( array("foo" => "bar")) ],
+			[ json_encode( array("data" => "bar")) ],
+			[ json_encode( array("data" => 1)) ],
+			[ json_encode( array("data" => false)) ],
+			[ json_encode( array("data" => true)) ],
+		);
+	}
+
+
 
 } 
