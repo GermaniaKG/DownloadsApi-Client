e6a0cf056418a2e5fddff30c044611aa171b558b
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-04-17 08:56:58 +0200

    Use httpbin.org

--- tests/src/GuzzleFactoryTest.php
+++ tests/src/GuzzleFactoryTest.php
@@ -9,12 +9,25 @@ class GuzzleFactoryTest extends \PHPUnit\Framework\TestCase
 
 	public function testFactory()
 	{
-		$api = $GLOBALS['DOWNLOADS_API'];
-		$token = $GLOBALS['AUTH_TOKEN'];
+		$api = "http://httpbin.org/bearer";
+		$token = "FooBar";
 
 		$sut = new GuzzleFactory;
 		$client = $sut($api, $token);
 
 		$this->assertInstanceOf( Client::class, $client);
+
+		// Perform test using httpbin.org:
+		// http://httpbin.org/#/Auth/get_bearer
+		$response = $client->get("");
+		$this->assertEquals(200, $response->getStatusCode());
+			
+		$response_decoded = json_decode($response->getBody()->__toString());
+		
+		$this->assertObjectHasAttribute("authenticated", $response_decoded);
+		$this->assertTrue($response_decoded->authenticated);
+
+		$this->assertObjectHasAttribute("token", $response_decoded);
+		$this->assertEquals($token, $response_decoded->token);
 	}
 }
