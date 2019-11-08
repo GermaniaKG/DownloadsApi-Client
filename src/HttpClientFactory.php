9763f08e5c00f821269d65771600f4aa2a1cb0a3
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2019-11-08 14:08:43 +0100

    Add HttpClientFactory

---
+++ src/HttpClientFactory.php
@@ -0,0 +1,35 @@
+<?php
+namespace Germania\DownloadsApiClient;
+
+use Psr\Http\Client\ClientInterface;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+
+
+/**
+ * This callable factory creates a Guzzle Client
+ * for usage with Germania KG's Downloads API.
+ */
+class HttpClientFactory
+{
+
+
+	/**
+	 * @param  string $api   The DownloadsApi endpoint
+	 * @param  string $token The AuthAPI Access token string
+	 * 
+	 * @return ClientInterface
+	 */
+	public function __invoke( string $api, string $token ) : ClientInterface
+	{
+		$auth_header = sprintf("Bearer %s", $token);
+
+		$headers = array(
+			'Authorization' => $auth_header
+		);
+
+		return GuzzleAdapter::createWithConfig([
+		    'base_uri' => $api,
+		    'headers'  => $headers
+		]);
+	}
+}
